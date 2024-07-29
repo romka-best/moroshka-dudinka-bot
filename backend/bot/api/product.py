6ad82d06d6fc614cf875563f7c30c200c8f2af6e
romka-best <romka.best2016@yandex.ru>
2024-07-28 19:19:49 -0700

    fourth commit for backend part

--- backend/bot/api/product.py
+++ backend/bot/api/product.py
@@ -1,11 +1,51 @@
-from fastapi import APIRouter
+import math
+from typing import Optional
 
-from bot.database.operations.product.getters import get_product_types
+from fastapi import APIRouter, Query
+
+from bot.database.operations.product.getters import get_product_types, get_products, get_product_type
 
 router = APIRouter(
     prefix="/products",
     tags=["product"],
 )
+
+
+@router.get("/products")
+async def get_all_products(
+    title: Optional[str] = None,
+    page: int = Query(1, gt=0),
+    size: int = Query(20, gt=0),
+):
+    all_products = await get_products()
+    products_by_page = await get_products(title, page * size, size)
+
+    result = []
+    for product in products_by_page:
+        product_type = await get_product_type(product.type_id)
+        result.append({
+            "id": product.id,
+            "title": product.title,
+            "description": product.description,
+            "cost": product.cost,
+            "weight": product.weight,
+            "photos": product.photos,
+            "type": {
+                "id": product_type.id,
+                "name": product_type.name,
+            },
+            "composition": product.composition,
+            "size": product.size,
+            "count": product.count,
+        })
+
+    return {
+        "items": result,
+        "page": page,
+        "pages": math.ceil(len(all_products) / size),
+        "size": len(result),
+        "total": len(all_products)
+    }
 
 
 @router.get("/products/types")
--- backend/bot/database/operations/product/getters.py
+++ backend/bot/database/operations/product/getters.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from google.cloud.firestore_v1 import FieldFilter
+from google.cloud.firestore_v1 import FieldFilter, Query
 
 from bot.database.main import firebase
 from bot.database.models.product import Product, ProductType
@@ -14,20 +14,38 @@ async def get_product(product_id: str) -> Optional[Product]:
         return Product(**product.to_dict())
 
 
-async def get_products() -> list[Product]:
-    products = firebase.db.collection(Product.COLLECTION_NAME).stream()
+async def get_products(title=None, offset=None, limit=None) -> list[Product]:
+    products_query = firebase.db.collection(Product.COLLECTION_NAME).order_by("created_at", direction=Query.ASCENDING)
+
+    if title:
+        products_query = products_query.order_by("title").start_at([title]).end_at([title + '\uf8ff'])
+    if offset:
+        products_query = products_query.offset(offset)
+    if limit:
+        products_query = products_query.limit(limit)
+
+    products = products_query.stream()
 
     return [
         Product(**product.to_dict()) async for product in products
     ]
 
 
-async def get_product_types() -> list[ProductType]:
-    product_types = (
-        firebase.db.collection(ProductType.COLLECTION_NAME)
-        .where(filter=FieldFilter("is_deleted", "==", False))
+async def get_product_type(product_type_id: str) -> Optional[ProductType]:
+    product_type_stream = firebase.db.collection(ProductType.COLLECTION_NAME) \
+        .where(filter=FieldFilter("id", "==", product_type_id)) \
+        .where(filter=FieldFilter("is_deleted", "==", False)) \
+        .limit(1) \
         .stream()
-    )
+
+    async for doc in product_type_stream:
+        return ProductType(**doc.to_dict())
+
+
+async def get_product_types() -> list[ProductType]:
+    product_types = firebase.db.collection(ProductType.COLLECTION_NAME) \
+        .where(filter=FieldFilter("is_deleted", "==", False)) \
+        .stream()
 
     return [
         ProductType(**product_type.to_dict()) async for product_type in product_types
