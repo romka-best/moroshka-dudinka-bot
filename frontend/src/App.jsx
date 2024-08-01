5e4e9479db8691294918a66cf50812d8e7c385e9
mixmaxmix <mixmaxmixmulty@gmail.com>
2024-08-01 19:39:52 +0300

    frontend: mobile test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import css from './App.module.scss';
 import { useEffect } from 'react';
+import css from './App.module.scss';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 
 const tg = window.Telegram.WebApp;
 
@@ -14,7 +15,18 @@ function App() {
   return (
     <>
       <div className={css['App-nav']}>
-
+        <div className={css['App-nav-item']}>
+          <ShoppingCartOutlined />
+          <p>каталог</p>
+        </div>
+        <div className={css['App-nav-item']}>
+          <ShoppingCartOutlined />
+          <p>корзина</p>
+        </div>
+        <div className={css['App-nav-item']}>
+          <ShoppingCartOutlined />
+          <p>корзина</p>
+        </div>
       </div>
     </>
   )
