ee21178b8ef999e5e5d832c22cf30036206e53b0
mixmaxmix <mixmaxmixmulty@gmail.com>
2024-08-01 23:13:59 +0300

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import css from './App.module.scss';
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Player } from '@lordicon/react';
+import CART from './assets/cart.json';
+import CATALOG from './assets/catalog.json';
+import PROFILE from './assets/profile.json';
 
 const tg = window.Telegram.WebApp;
 
 console.log(tg)
 
 function App() {
+  const catalogRef = useRef(null);
+  const cartRef = useRef(null);
+  const profileRef = useRef(null);
 
   useEffect(() => {
     tg.ready();
+    tg.expand();
   }, []);
 
   return (
     <>
       <div className={css['App-nav']}>
-        <div className={css['App-nav-item']}>
-          <ShoppingCartOutlined />
+        <div className={css['App-nav-item']} onClick={() => catalogRef.current?.playFromBeginning()}>
+          <Player
+            ref={catalogRef}
+            icon={CATALOG}
+            size={32}
+          />
           <p>каталог</p>
         </div>
         <div className={css['App-nav-item']}>
-          <ShoppingCartOutlined />
+          <Player
+            ref={cartRef}
+            icon={CART}
+            size={32}
+          />
           <p>корзина</p>
         </div>
         <div className={css['App-nav-item']}>
-          <ShoppingCartOutlined />
-          <p>корзина</p>
+          <Player
+            ref={profileRef}
+            icon={PROFILE}
+            size={32}
+          />
+          <p>профиль</p>
         </div>
       </div>
     </>
