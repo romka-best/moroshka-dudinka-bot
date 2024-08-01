f03cf8550ad8d177972aef6bd82184239a5ca206
mixmaxmix <mixmaxmixmulty@gmail.com>
2024-08-01 18:53:16 +0300

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css';
+import css from './App.module.scss';
+import { useEffect } from 'react';
+
+const tg = window.Telegram.WebApp;
+
+useEffect(() => {
+  tg.ready();
+}, []);
+
+console.log(tg)
 
 function App() {
 
   return (
     <>
-      
+      <div className={css['App-nav']}>
+
+      </div>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 import ruRU from 'antd/es/locale/ru_RU';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, App } from 'antd';
 import { Provider } from 'react-redux';
 import store from './store';
+import Router from './router/Router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <ConfigProvider
       locale={ruRU}
     >
-      <App />
-
+      <App>
+        <Router />
+      </App>
     </ConfigProvider>
   </Provider>,
-)
+);
